f0437be2cb4b845354689000bc49fbaba14ca191
soumik-mondal <64778804+soumik-mondal@users.noreply.github.com>
2025-12-25 23:37:50 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,51 +1,62 @@
 const CACHE_NAME = 'soham-study-schedule-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './icon-192x192.png',
-  './icon-512x512.png'
-];
 
 // Install service worker
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+    console.log('Service Worker: Installing...');
+    
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Service Worker: Caching app shell');
+                // Only cache essential files that we know exist
+                return cache.addAll([
+                    './',
+                    './index.html'
+                ]).catch(err => {
+                    console.log('Service Worker: Cache addAll error:', err);
+                });
+            })
+    );
 });
 
 // Fetch resources
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
+    console.log('Service Worker: Fetching', event.request.url);
+    
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Return cached response if found
+                if (response) {
+                    console.log('Service Worker: Serving from cache:', event.request.url);
+                    return response;
+                }
+                
+                // Otherwise fetch from network
+                console.log('Service Worker: Fetching from network:', event.request.url);
+                return fetch(event.request);
+            })
+            .catch(error => {
+                console.log('Service Worker: Fetch failed:', error);
+                // You could return a custom offline page here
+            })
+    );
 });
 
-// Update service worker
+// Clean up old caches
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
+    console.log('Service Worker: Activating...');
+    
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Service Worker: Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
-      );
-    })
-  );
+    );
 });
